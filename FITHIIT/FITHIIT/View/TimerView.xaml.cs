43a477c28a33497d6f0f9af69decfe8e52fab078
Daniel Tran <daniel_t4221@hotmail.com>
2018-08-14 23:08:27 +1200

    Added timer model, view and view model files

---
+++ FITHIIT/FITHIIT/View/TimerView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FITHIIT.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TimerView : ContentPage
+	{
+		public TimerView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ FITHIIT/FITHIIT/ViewModel/TimerViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FITHIIT.ViewModel
+{
+    class TimerViewModel
+    {
+    }
+}
